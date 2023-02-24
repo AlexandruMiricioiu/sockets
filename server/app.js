61d92577b42cee093d51beb99ab6bbb70aac632f
Alexandru Miricioiu <alexandrumiricioiu@gmail.com>
2023-02-25 01:01:11 +0200

    add socket.io

--- server/app.js
+++ server/app.js
@@ -8,17 +8,23 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(
-  cors({
-    credentials: true,
-    // origin: "http://localhost:3000",
-  })
-)
+// app.use(
+//   cors({
+//     credentials: true,
+//     // origin: "http://localhost:3000",
+//   })
+// )
 
 const routes  = require('./routes/routes')
 
 app.use(routes)
 
-
 const port = 4000
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', () => {
+  console.log('io connected')
+})
+
+server.listen(port, () => console.log(`Server started on port ${port}`))
